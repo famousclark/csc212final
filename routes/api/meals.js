649405685c43003735dfd5636258d622817ed173
KelvinFerreiras <kferrei3@u.rochester.edu>
2019-04-16 15:46:01 -0400

    API Routes for Users, Restaurants and Meals installed to new server

---
+++ routes/api/meals.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+var ctrlMeal= require('../../controllers/meal.controller');
+
+router.get('/get/all', ctrlMeal.getAllMeals); /* Returns all meals. JSON body payload format :: {}*/
+router.get('/get', ctrlMeal.getMeal); /* Returns a restaurant matching the given meal id. JSON body payload format :: {"meal_id":"someString"}*/
+router.post('/add', ctrlMeal.saveMeal); /* Adds a new meal. JSON body payload format :: {"meal_id":"someString", "name":"someString", "description":"someString", "r_code":"someString", "dinning_hall_item":"someString" , "allergens":[Array of Strings], "diet_restrinctions":[Array of Strings], "serve_amount":"a number", "nutrition":{Object (see model)}, "image":"someString", "price":"someString"}*/
+router.post('/edit', ctrlMeal.editMeal); /* Edits a meal matching the given meal id. JSON body payload format :: { "name":"someString", "description":"someString", "r_code":"someString", "dinning_hall_item":"someString" , "allergens":[Array of Strings], "diet_restrinctions":[Array of Strings], "serve_amount":"a number", "nutrition":{Object (see model)}, "image":"someString", "price":"someString"}*/
+router.post('/delete', ctrlMeal.deleteMeal); /* Deletes a meal matching the given meal id. JSON body payload format :: {"meal_id":"someString"}*/
+
+
+
+module.exports = router;
